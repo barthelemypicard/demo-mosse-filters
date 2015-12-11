3da423fc478b40eafa218e82759cb175191ee5f2
barthelemypicard <barthelemy.picard@supelec.fr>
2015-12-11 13:23:31 +0100

    Files added.

---
+++ mosse_tracking.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 07 11:21:21 2015
+
+@author: Bart
+"""
+
+from __future__ import division
+import utils
+import numpy as np
+import pylab as pl
+import scipy
+from scipy import fftpack
+
+
+TPL_RECT        = None
+COS_WINDOW      = None
+
+FILTER_INIT     = False
+FILTER_NUM      = 0.0
+FILTER_DEN      = 1.0
+FILTER_STD      = 2.0
+LEARNING_RATE   = 0.125
+REG_FACTOR      = 0.01
+TPL_CENTER      = np.zeros(2)
+
+
+def initFilter(tpl):
+    global COS_WINDOW
+    global FILTER_INIT
+    global FILTER_STD
+    global FILTER_NUM
+    global FILTER_DEN
+    
+    height, width = tpl.shape
+    ptpl = tpl * COS_WINDOW
+    output = utils.genGaussianMatrix(width, height, (width/2, height/2), FILTER_STD)
+    ftpl = np.fft.fft2(ptpl)
+    foutput = np.fft.fft2(output)
+    FILTER_NUM = foutput * np.conj(ftpl)
+    FILTER_DEN = ftpl * np.conj(ftpl)
+    FILTER_INIT = True
+
+def tplCback(pts):
+    global TPL_RECT
+    global COS_WINDOW
+    global FILTER_INIT
+    
+    if TPL_RECT == None:
+        TPL_RECT = pts
+        width = pts[1, 1] - pts[0, 1]
+        height = pts[1, 0] - pts[0, 0]
+        cos_col = np.sin(np.pi*np.arange(0, height)/(height-1.0)).reshape((height, 1))
+        cos_row = np.sin(np.pi*np.arange(0, width)/(width-1.0)).reshape((1, width))
+        COS_WINDOW = cos_col * cos_row
+        FILTER_INIT = False
+        print pts
+    
+    
+def updateFilter(tpl):
+    global COS_WINDOW
+    global FILTER_STD
+    global FILTER_NUM
+    global FILTER_DEN
+    global LEARNING_RATE
+    
+    height, width = tpl.shape
+    ptpl = tpl * COS_WINDOW
+    output = utils.genGaussianMatrix(width, height, (width/2, height/2), FILTER_STD)
+    ftpl = np.fft.fft2(ptpl)
+    foutput = np.fft.fft2(output)
+    FILTER_NUM = LEARNING_RATE * (foutput * np.conj(ftpl)) + (1.0 - LEARNING_RATE) * FILTER_NUM
+    FILTER_DEN = LEARNING_RATE * (ftpl * np.conj(ftpl))+ (1.0 - LEARNING_RATE) * FILTER_DEN
+    
+    
+def processFrame(im):
+    utils.preprocessing(im)
+    global TPL_RECT
+    global TPL_CENTER
+    global FILTER_INIT
+    global FILTER_NUM
+    global FILTER_DEN
+    global COS_WINDOW
+    
+    if TPL_RECT != None:
+        tplc = TPL_RECT.astype(np.uint32)
+        patch = im[tplc[0,0]:tplc[1,0], tplc[0,1]:tplc[1,1]];
+        if not FILTER_INIT:
+            initFilter(patch)
+            print tplc.shape
+        else:
+            if patch.shape == COS_WINDOW.shape:
+                height, width = patch.shape
+#                ptpl = patch * COS_WINDOW
+#                height, width = ptpl.shape
+#                output = utils.genGaussianMatrix(width, height, (width/2, height/2), 2.0)
+#                ftpl = np.fft.fft2(ptpl)
+#                foutput = np.fft.fft2(output)
+#                n = foutput #* np.conj(ftpl)
+#                d = ftpl #* np.conj(ftpl)
+                G = np.conj(FILTER_NUM/FILTER_DEN) * np.conj(np.fft.fft2(patch * COS_WINDOW))
+                g = np.real(np.fft.ifft2(G))
+                utils.showImage('output', g);
+                utils.showImage('filter', np.real(np.fft.fftshift(np.fft.ifft2(np.conj(FILTER_NUM/FILTER_DEN)))));
+                peak_pos = np.argmax(g);
+                dy = peak_pos // width - height//2
+                dx = peak_pos % width - width//2
+                tplc[:,0] -= dy
+                tplc[:,1] -= dx
+                TPL_RECT[:,0] -= dy
+                TPL_RECT[:,1] -= dx
+                new_patch = im[tplc[0,0]:tplc[1,0], tplc[0,1]:tplc[1,1]];
+                updateFilter(new_patch)
+
+        utils.drawRectangle(im, tplc)
+    
+    return im
+
+
+if __name__ == "__main__":
+    utils.initTemplateCapture(tplCback)
+    utils.runCamLoop(processFrame)
---
+++ utils.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 07 10:53:27 2015
+
+@author: Bart
+"""
+
+from __future__ import division
+import numpy as np
+import pylab as pl
+import scipy
+import cv2
+
+
+RECT_PTS    = np.zeros((2,2))
+RECT_CBACK  = None
+
+
+
+def preprocessing(im):
+    res = im.astype(np.float64)
+    res = np.log(res+1.0)
+    res -= res.mean()
+    res /= res.max()
+    
+    return res
+
+
+def genGaussianMatrix(width, height, center, sigma):
+    gauss_row = np.exp(-(np.arange(0,width) - center[0])**2/(2*sigma**2))
+    gauss_col = np.exp(-(np.arange(0,height) - center[1])**2/(2*sigma**2))
+    gauss_row = gauss_row.reshape((1, width))
+    gauss_col = gauss_col.reshape((height, 1))
+    res = gauss_col * gauss_row
+    res /= 2*np.pi*sigma**2
+    
+    return res
+
+
+def showImage(title, im):
+    im = (im - np.min(im))
+    im /= np.max(im)
+    im *= 255
+    im = im.astype(np.uint8)
+    cv2.imshow(title, im)
+
+
+def runCamLoop(cback, camid = 0):
+    cap = cv2.VideoCapture(camid)
+    while(True):
+        ret, frame = cap.read()
+        im = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        im = cback(im)
+        
+        cv2.imshow('frame', im)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def captureRectangle(event, x, y, flags, param):
+    global RECT_PTS
+    global RECT_CBACK
+    
+    if event == cv2.EVENT_LBUTTONDOWN:
+        RECT_PTS[0,0] = y
+        RECT_PTS[0,1] = x
+    elif event == cv2.EVENT_LBUTTONUP:
+        RECT_PTS[1,0] = y
+        RECT_PTS[1,1] = x
+        RECT_CBACK(RECT_PTS)
+        
+
+def initTemplateCapture(cback):
+    global RECT_CBACK
+    RECT_CBACK = cback
+    cv2.namedWindow('frame')
+    cv2.setMouseCallback('frame', captureRectangle)
+
+
+def drawRectangle(im, pts):
+    cv2.rectangle(im, (pts[0,1],pts[0,0]),(pts[1,1],pts[1,0]), (0, 255, 0), 2)
+    
+    
+    
+if __name__ == "__main__":
+    im = scipy.misc.lena();
+    print im.shape
+    pl.imshow(preprocessing(im))
+    pl.gray()
+    pl.show()
+    runCamLoop(preprocessing)
